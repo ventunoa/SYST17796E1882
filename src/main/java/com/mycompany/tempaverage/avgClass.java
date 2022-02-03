cc25812cb2c79863bd09b1d4900aa0336aa13682
ventunoa <ventunoa@sheridancollege.ca>
2022-02-03 13:34:08 -0500

    added maxCalc class

--- src/main/java/com/mycompany/tempaverage/avgClass.java
+++ src/main/java/com/mycompany/tempaverage/avgClass.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 
-package com.mycompany.tempaverage;
+package com.mycompany.tempavg;
+
 /**
  *
  * @author Noah
@@ -15,9 +16,11 @@ public class avgClass {
 public static void main(String[] args){
     int []  temps = {12,24,56,9,-4,-8,-55,101,22,-4,33,13,0,-40};
     System.out.println("Temperature average is "+calcAvg(temps));
+    System.out.println("Max temp is "+calcMax(temps));
 }
 
 public static int calcAvg (int [] temps){
+
         int avg = 0;
         int incorrectCount = 0;
         for (int i=0;i<temps.length;i++) {
@@ -26,9 +29,28 @@ public static int calcAvg (int [] temps){
         		continue;
         	}
         	else
-        		avg += temps[i];	
+        		avg += temps[i];
+        	
         }
         avg = avg/(temps.length - incorrectCount);
-		return avg;    
+		return avg;
+        
 }
+
+public static int calcMax (int [] temps){
+    int max = temps[0];
+    for (int i=0;i<temps.length;i++) {
+        if (temps[i] <50 && temps[i]>-50) {
+            if(temps[i]>max) {
+                max=temps[i];
+            }
+        }
+   }   
+    
+    return max;
+}
+
+
+
+
 }
